0fcbdf73714aa637ca025d4c0e74c2347235a175
natigalo <natigalo1@gmail.com>
2023-05-15 19:37:24 -0500

    config mongo

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import "dotenv/config.js"
+import './config/database.js'
 
 const server= express(); //crear servidor
-
 const PORT = process.env.PORT || 8080; //definir el puerto
-
 const ready = () => console.log('ready on '+ PORT);
-
-server.listen(PORT, ready);
-
 console.log(process.env.NODE_ENV);
 
+//midelwares
+server.use(express.json()) //permite entradas y tambien trabajar con formato json
+server.use(express.urlencoded({ extended: false })) //permite capturar consultas complejas
+
+//router + server
+server.listen(PORT, ready);
