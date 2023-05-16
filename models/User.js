d1e66493f4c7a6896fff009f1a124061a8b85cf5
natigalo <natigalo1@gmail.com>
2023-05-15 21:08:27 -0500

    minga 3

--- models/User.js
+++ models/User.js
@@ -1,15 +1,15 @@
-import { schema, model, Types, Schema } from 'mongoose'
+import { Schema, model} from 'mongoose'
 let collection = 'users'
 let schema = new Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
     photo: { type: String, required: true },
-    role: { type: numeber, required: true },
+    role: { type: Number, required: true },
     online: { type: Boolean, required: true },
 
 }, {
     timestamps: true,
 
 })
-let User = model(collection, Schema)
-export default user 
+let User = model(collection, schema)
+export default User 
