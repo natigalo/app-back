fb16f7ddf35abe15a6f505e9792479d3547080e6
natigalo <natigalo1@gmail.com>
2023-05-16 21:16:38 -0500

    fix

--- models/Manga.js
+++ models/Manga.js
@@ -2,7 +2,7 @@ import{Schema, model, Types} from "mongoose"
 let collection= 'mangas'
 
 let schema= new Schema({
-    author_id:{type: Types.ObjectId, ref:'authors' required: true},
+    author_id:{type: Types.ObjectId, ref:'authors', required: true},
     company_id:{type: String, required: false},
     title: {type: String, required: true},
     cover_photo: {type: String, required: true},
